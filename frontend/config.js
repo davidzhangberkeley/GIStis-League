57adcdc864610b8d3c62bb7d80276a587e089c58
Joshua Chen <joshuachen910805@gmail.com>
2026-04-06 20:26:10 -0700

    Point frontend to deployed backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 const ALEX_MAPBOX_TOKEN = "pk.eyJ1IjoiYWxvLTk3IiwiYSI6ImNtbTFqN3RuYjBhNzgycnB4YTQ0NGg1dmYifQ.gKIQJj5S9Pw1tgJUY_fb9g";
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://gistis-league.onrender.com";
